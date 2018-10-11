4fff5ae6c0e2a9a78ec09a06b60362edfaa90954
Siva-Weboffice <siva@webofficeit.com>
2018-10-11 20:03:27 +0530

    general fixes

--- resources/views/frontend/catalog/category.blade.php
+++ resources/views/frontend/catalog/category.blade.php
@@ -7,15 +7,20 @@
      
 
     <div class="card">
-    <div class="card-body"> 
-      @foreach($category as $category)
-      <div class="row">
-           <a href='{{ url($category->seo.'/offer') }}'>{{ $category->seo }}</a>
-           
-          <img src='{{ url('storage/category/'.$category->user_id.'/'.$category->picture) }}'>
-      </div>
-      @endforeach
-    </div>    
+        <div class="card-body"> 
+            <div class="category-list-view list-view">
+                <div class="row">
+                    @foreach($category as $category)
+                        <div class="col-lg-3">
+                            <div class="caegory-list-item">
+                                <img src='{{ url('storage/category/'.$category->user_id.'/'.$category->picture) }}' class="img-fluid" >
+                                <a href='{{ url($category->seo.'/offers') }}'>{{ $category->seo }}</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>    
     </div>
     
 @endsection
