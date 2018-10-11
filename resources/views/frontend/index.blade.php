87aa01b3d4c7dd38573bd14e8da02bb6f6c26ee8
Siva-Weboffice <siva@webofficeit.com>
2018-10-11 15:54:39 +0530

    homepage table style

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,34 +10,35 @@
     <div id="chartdiv"></div>  
 
     <div class="row">
-      
-        <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th>@lang('labels.backend.access.users.table.last_name')</th>
-                            <th>@lang('labels.backend.access.users.table.first_name')</th>
-                            <th>@lang('labels.backend.access.users.table.email')</th>
-                            
-                            <th>@lang('labels.backend.access.users.table.accountype')</th>
-                            
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                          
-                            <tr>
-                                <td>{{ $user->last_name }}</td>
-                                <td>{{ $user->first_name }}</td>
-                                <td><a href="{{ url('category/'.Crypt::encryptString($user->id)) }}">{{ $user->email }}</a></td>
-                                <td>{{ $user->accounttype? $user->accounttype->name : '' }}</td>
-                              
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>                             
- <div class="row">
+      <div class="col-12">
+        <div class="table-responsive recent-users">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                <tr>
+                    <th>@lang('labels.backend.access.users.table.last_name')</th>
+                    <th>@lang('labels.backend.access.users.table.first_name')</th>
+                    <th>@lang('labels.backend.access.users.table.email')</th>
+                    
+                    <th>@lang('labels.backend.access.users.table.accountype')</th>
+                    
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    
+                    <tr>
+                        <td>{{ $user->last_name }}</td>
+                        <td>{{ $user->first_name }}</td>
+                        <td><a href="{{ url('category/'.Crypt::encryptString($user->id)) }}">{{ $user->email }}</a></td>
+                        <td>{{ $user->accounttype? $user->accounttype->name : '' }}</td>
+                        
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+      </div>
+        <div class="row">
             <div class="col-7">
                 <div class="float-left">
                     {!! $users->count() !!} {{ trans_choice('labels.backend.access.users.table.total', $users->total()) }} {!! $users->total() !!}
